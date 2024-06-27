cfaad061cb15fb447234cf49534a303fd7bbb388
Moiez Qamar <89601171+moqm25@users.noreply.github.com>
2024-06-27 01:58:48 -0400

    styling

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ window.onload = function () {
 		gameImage.style.opacity = "1";
 	});
 };
+
+window.onload = function () {
+	var recommendation = document.getElementById("recommendation");
+	var osName = "Unknown OS";
+	if (navigator.appVersion.indexOf("Win") != -1) osName = "Windows";
+	if (navigator.appVersion.indexOf("Mac") != -1) osName = "MacOS";
+
+	var recommendationText = "<h2>Recommended Download for " + osName + "</h2>";
+	if (osName == "Windows") {
+		recommendationText +=
+			'<a href="https://github.com/gianellaf56/LearnEnglishNow/blob/main/windowsos/Learn%20English%20Now.zip" download>Download</a>';
+	} else if (osName == "MacOS") {
+		recommendationText +=
+			'<a href="https://github.com/gianellaf56/LearnEnglishNow/blob/main/macos/Learn%20English%20Now.zip" download>Download</a>';
+	} else {
+		recommendationText +=
+			"<p>Please choose the appropriate download for your operating system below.</p>";
+	}
+
+	recommendation.innerHTML = recommendationText;
+};
